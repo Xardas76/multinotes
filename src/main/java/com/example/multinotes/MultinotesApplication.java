609ab302915def7c26c269433f2b8a639b74693e
Ivan Diachenko <ivan.diachenko@ittc.ru>
2024-12-05 13:02:37 +0100

    init

---
+++ src/main/java/com/example/multinotes/MultinotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.multinotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultinotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultinotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/multinotes/MultinotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multinotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultinotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
